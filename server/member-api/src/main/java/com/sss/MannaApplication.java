9a472e08ca57087fa5baac525f4ece2b7e313664
ds <2601948@gmail.com>
2022-06-30 00:14:23 +0900

    set to multi-module project

--- server/member-api/src/main/java/com/sss/MannaApplication.java
+++ server/member-api/src/main/java/com/sss/MannaApplication.java
@@ -1,4 +1,4 @@
-package com.sss.manna;
+package com.sss;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ server/member-api/src/test/java/com/sss/MannaApplicationTest.java
@@ -0,0 +1,12 @@
+package com.sss;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MannaApplicationTest {
+
+    @Test
+    void main() {
+    }
+}
