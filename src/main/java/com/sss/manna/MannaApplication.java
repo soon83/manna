bda63f59b128c48321743cabe3c2017f3fa7696b
ds <2601948@gmail.com>
2022-06-29 23:59:02 +0900

    create project

---
+++ src/main/java/com/sss/manna/MannaApplication.java
@@ -0,0 +1,13 @@
+package com.sss.manna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MannaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MannaApplication.class, args);
+    }
+
+}
