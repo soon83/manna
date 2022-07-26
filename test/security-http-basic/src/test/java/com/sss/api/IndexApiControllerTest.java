86e20b20f19a432092e0e2f67e1f1bd61627d96e
ds <2601948@gmail.com>
2022-07-26 09:22:28 +0900

    add: request header, Authorization 에 유효하지않은 사용자정보를 넣는 테스트

--- test/security-http-basic/src/test/java/com/sss/api/IndexApiControllerTest.java
+++ test/security-http-basic/src/test/java/com/sss/api/IndexApiControllerTest.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.MediaType;
-import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
@@ -19,7 +18,8 @@ import java.nio.charset.StandardCharsets;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class IndexApiControllerTest {
     private WebTestClient webTestClient;
-    private static String BASIC_TOKEN = "Basic" + Base64.encodeBase64String("user:1234".getBytes(StandardCharsets.UTF_8));
+    private static String VALID_BASIC_TOKEN = "Basic" + Base64.encodeBase64String("user:1234".getBytes(StandardCharsets.UTF_8));
+    private static String INVALID_BASIC_TOKEN = "Basic" + Base64.encodeBase64String("user:0000".getBytes(StandardCharsets.UTF_8));
 
     @LocalServerPort
     private Integer port;
@@ -36,7 +36,7 @@ public class IndexApiControllerTest {
     public void callUserApiByUser() {
         this.webTestClient.get().uri("/api/user")
                 .accept(MediaType.APPLICATION_JSON)
-                .header("Authorization", BASIC_TOKEN)
+                .header("Authorization", VALID_BASIC_TOKEN)
                 .exchange()
                 .expectStatus().isOk();
     }
@@ -44,12 +44,20 @@ public class IndexApiControllerTest {
     @Test
     @DisplayName("[http-basic] user 가 admin API 를 호출")
     public void callAdminApiByUser() {
-        String basicToken = Base64.encodeBase64String("user:1234".getBytes(StandardCharsets.UTF_8));
-
         this.webTestClient.get().uri("/api/admin")
                 .accept(MediaType.APPLICATION_JSON)
-                .header("Authorization", BASIC_TOKEN)
+                .header("Authorization", VALID_BASIC_TOKEN)
                 .exchange()
                 .expectStatus().isForbidden();
+    }
+
+    @Test
+    @DisplayName("[http-basic] id:password 가 일치하지 않을 때")
+    public void invalidUserInfo() {
+        this.webTestClient.get().uri("/api/user")
+                .accept(MediaType.APPLICATION_JSON)
+                .header("Authorization", INVALID_BASIC_TOKEN)
+                .exchange()
+                .expectStatus().isUnauthorized();
     }
 }
